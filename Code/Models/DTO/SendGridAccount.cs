2666ed43a145b623ae00bbab5870aa3fe71a83a7
layla-p <layla.porter@gmail.com>
2020-06-04 13:12:13 +0100

    created base level

--- Code/Models/DTO/SendGridAccount.cs
+++
@@ -1,8 +0,0 @@
-namespace CloudCityCakeCo.Models.DTO
-{
-    public class SendGridAccount
-    {
-        public string ApiKey { get; set; }
-        public string EmailFromAddress { get; set; }
-    }
-}
--- Code/Models/DTO/TwilioAccount.cs
+++
@@ -1,8 +0,0 @@
-namespace CloudCityCakeCo.Models.DTO
-{
-    public class TwilioAccount
-    {
-        public string AccountSid { get; set; }
-        public string AuthToken { get; set; }
-    }
-}
