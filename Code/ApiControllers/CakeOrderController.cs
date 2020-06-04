d62a31cc088f2c59c11cfa2e5c51f93da6c9f89d
layla-p <layla.porter@gmail.com>
2020-06-04 13:16:47 +0100

    start of video three

--- Code/ApiControllers/CakeOrderController.cs
+++ Code/ApiControllers/CakeOrderController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading.Tasks;
+using CloudCityCakeCo.Data;
 using CloudCityCakeCo.Models.DTO;
-using CloudCityCakeCo.Services.Interfaces;
+using CloudCityCakeCo.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CloudCityCakeCo.ApiControllers
 {
@@ -10,19 +12,60 @@ namespace CloudCityCakeCo.ApiControllers
     [Route("api/order")]
     public class CakeOrderController : ControllerBase
     {
-        private readonly ICakeOrderService _cakeOrderService;
-        public CakeOrderController(ICakeOrderService cakeOrderService)
+        private readonly ApplicationDbContext _context;
+        public CakeOrderController(ApplicationDbContext context)
         {
-            _cakeOrderService = cakeOrderService ?? throw new ArgumentNullException(nameof(cakeOrderService));
+            _context = context;
         }
- 
+        
+        // GET
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return null;
+        }
+        
+        
         // POST
         [HttpPost]
         public async Task<IActionResult> Post(OrderDetails orderDetails)
         {
-            var cakeOrderResponse = await _cakeOrderService
-                .AddNewOrderAsync(orderDetails);
-            return new JsonResult(cakeOrderResponse.Content.Id.ToString());
+            var user = await _context.Users
+                .FirstOrDefaultAsync(e => e.Number == orderDetails.Number);
+
+            if (user == null)
+            {
+                user = new User
+                {
+                    Name = orderDetails.Name,
+                    Number = orderDetails.Number,
+                    Email = orderDetails.Email
+                };
+
+                var entity = await _context.Users.AddAsync(user);
+
+                await _context.SaveChangesAsync();
+                user = entity.Entity;
+            }
+            
+            var cakeOrder = new CakeOrder
+            {
+                Flavour = orderDetails.Cake.Flavour,
+                Frosting = orderDetails.Cake.Frosting,
+                Topping = orderDetails.Cake.Topping,
+                Size = orderDetails.Cake.Size,
+                Price = orderDetails.Cake.Price,
+                OrderDate = DateTime.UtcNow,
+                UserId =  user.Id
+            };
+
+            var cakeEntity = await _context
+                .CakeOrders.AddAsync(cakeOrder);
+
+            await _context.SaveChangesAsync();
+            
+            
+            return new JsonResult(cakeEntity.Entity.Id.ToString());
         }
     }
 }
